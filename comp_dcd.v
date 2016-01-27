5f3ba69cc04aa7c4a3f253832018bb9b3389aaf2
ncarnevale <ncarnevale12@gmail.com>
2016-01-27 15:37:47 -0600

    Fixed comp_dcd
    Now comp_dcd takes in a less than signal instead of greater than. Note
    the changes in the control signals on top of the file.

--- comp_dcd.v
+++ comp_dcd.v
@@ -3,38 +3,38 @@
 * Returns 32 bit 0 or 1 according to comp request
 *
 * Ctrl designations:
-* 000 = set on less than
-* 001 = set on less than or equal to
-* 010 = set on greater than
-* 011 = set on greater than or equal to
+* 000 = set on greater than
+* 001 = set on greater than or equal to
+* 010 = set on less than
+* 011 = set on less than or equal to
 * 1X0 = set on equal to
 * 1X1 = set on not equal to
 */
 
-module comp_dcd(grtr_in, eql_in, ctrl, z);
+module comp_dcd(less_in, eql_in, ctrl, z);
 
 input [2:0] 	ctrl;
-input 		grtr_in;
+input 		less_in;
 input 		eql_in;
 output [31:0]	z;
 
 wire [2:0] 	ctrl;
-wire 		grtr_in;
+wire 		less_in;
 wire 		eql_in;
 wire [31:0]	z;
 
 wire comp1, comp2, comp3, eq1, sel1;
-wire less1, less2, less3, grtr1;
+wire less1, grtr2, grtr3, grtr1;
 
 and_1b c1 (.a (ctrl[0]), .b (eql_in), .z (comp1));
-//xor_1b c2 (.a (ctrl[1]), .b (grtr_in), .z (comp2));
-and_1b c2 (.a (ctrl[1]), .b (grtr_in), .z (grtr1));
+//xor_1b c2 (.a (ctrl[1]), .b (less_in), .z (comp2));
+and_1b c2 (.a (ctrl[1]), .b (less_in), .z (less1));
 
-or_1b l1 (.a (ctrl[1]), .b (eql_in), .z (less1));
-or_1b l2 (.a (less1), .b (grtr_in), .z (less2));
-not_1b l3 (.a (less2), .z (less3));
+or_1b l1 (.a (ctrl[1]), .b (eql_in), .z (grtr1));
+or_1b l2 (.a (grtr1), .b (less_in), .z (grtr2));
+not_1b l3 (.a (grtr2), .z (grtr3));
 
-or_1b lc (.a (less3), .b (grtr1), .z (comp2));
+or_1b lc (.a (grtr3), .b (less1), .z (comp2));
 
 or_1b c3 (.a (comp1), .b(comp2), .z (comp3));
 
--- comp_dcd_tb.v
+++ comp_dcd_tb.v
@@ -1,27 +1,27 @@
 module comp_dcd_tb;
-  reg 		grtr_in;
+  reg 		less_in;
   reg 		eql_in;
   reg [2:0] 	ctrl;
   wire [31:0]	z;
 
-  comp_dcd cd(.grtr_in(grtr_in), .eql_in(eql_in), .ctrl(ctrl), .z(z));
+  comp_dcd cd(.less_in(less_in), .eql_in(eql_in), .ctrl(ctrl), .z(z));
 
    initial begin
       $display("These tests should pass (Z = 1)");
-      $monitor("CTRL = %b GRTR = %b EQL = %b Z = %d", ctrl, grtr_in, eql_in, z);  
-      #0 ctrl = 3'b000; grtr_in = 0; eql_in = 0;   // less than
-      #10 ctrl = 3'b001; grtr_in = 0; eql_in = 1;  // less than or equal
-      #10 ctrl = 3'b010; grtr_in = 1; eql_in = 0;   // greater than
-      #10 ctrl = 3'b011; grtr_in = 1; eql_in = 1;  // greater than or equal
-      #10 ctrl = 3'b100; grtr_in = 0; eql_in = 1;   // equal
-      #10 ctrl = 3'b101; grtr_in = 1; eql_in = 0;   // not equal
+      $monitor("CTRL = %b LESS = %b EQL = %b Z = %d", ctrl, less_in, eql_in, z);  
+      #0 ctrl = 3'b000; less_in = 0; eql_in = 0;   // greater than
+      #10 ctrl = 3'b001; less_in = 0; eql_in = 1;  // greater than or equal
+      #10 ctrl = 3'b010; less_in = 1; eql_in = 0;   // less than
+      #10 ctrl = 3'b011; less_in = 1; eql_in = 1;  // less than or equal
+      #10 ctrl = 3'b100; less_in = 0; eql_in = 1;   // equal
+      #10 ctrl = 3'b101; less_in = 1; eql_in = 0;   // not equal
       $display("These tests should fail (Z = 0)");
-      #0 ctrl = 3'b000; grtr_in = 0; eql_in = 1;   // less than
-      #10 ctrl = 3'b001; grtr_in = 1; eql_in = 0;  // less than or equal
-      #10 ctrl = 3'b010; grtr_in = 0; eql_in = 1;   // greater than
-      #10 ctrl = 3'b011; grtr_in = 0; eql_in = 0;  // greater than or equal
-      #10 ctrl = 3'b100; grtr_in = 0; eql_in = 0;   // equal
-      #10 ctrl = 3'b101; grtr_in = 0; eql_in = 1;   // not equal
+      #0 ctrl = 3'b000; less_in = 0; eql_in = 1;   // greater than
+      #10 ctrl = 3'b001; less_in = 1; eql_in = 0;  // greater than or equal
+      #10 ctrl = 3'b010; less_in = 0; eql_in = 1;   // less than
+      #10 ctrl = 3'b011; less_in = 0; eql_in = 0;  // less than or equal
+      #10 ctrl = 3'b100; less_in = 0; eql_in = 0;   // equal
+      #10 ctrl = 3'b101; less_in = 0; eql_in = 1;   // not equal
    end // initial begin
 
    endmodule
